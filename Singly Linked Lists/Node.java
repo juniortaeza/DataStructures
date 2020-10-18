9eb04537f1c33b5f578776f3b673a884397ed4ee
helloiamjunior <htaeza295@live.com>
2020-10-17 22:00:42 -0700

    Node - Responsible for Nodes created in Linked List class

---
+++ Singly Linked Lists/Node.java
@@ -0,0 +1,11 @@
+/*
+@Author: Heginio Jr Taeza
+@GitHub: juniortaeza
+*/
+package singly.linked.lists;
+
+public class Node<T> //set Node class of generic type T
+{
+    protected T Data; //create an instance of generic type T called Data
+    protected Node Next; //create an instance of Node called Next
+}
